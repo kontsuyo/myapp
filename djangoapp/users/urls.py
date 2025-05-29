d58932d56be244587a37c2983feaa246e9815812
Chang Doe <kontsuyo@users.noreply.github.com>
2025-05-29 17:41:21 +0900

    refactor: viewの名前変更

--- djangoapp/users/urls.py
+++ djangoapp/users/urls.py
@@ -2,19 +2,17 @@ from django.urls import path
 
 from users.views import (
     AccountDeleteView,
+    AccountRegisterView,
     AccoutUpdateView,
     LoginView,
     ProfileView,
-    RegisterView,
 )
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),  # !signupに変更
-    path("login/", LoginView.as_view(), name="login"),
-    path(
-        "update_account/", AccoutUpdateView.as_view(), name="account-update"
-    ),  # !update_accountに変更
+    path("register/", AccountRegisterView.as_view(), name="register"),  # !signupに変更
+    path("update_account/", AccoutUpdateView.as_view(), name="account-update"),  # !update_accountに変更
     path("delete_account/", AccountDeleteView.as_view(), name="delete-account"),
+    path("login/", LoginView.as_view(), name="login"),
     # path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", ProfileView.as_view(), name="profile"),
     # path("update_profile/", UpdateProfileView.as_view(), name="update_profile"),
