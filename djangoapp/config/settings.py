ecff04bafa8f225d2f2dab9a6a221cf51786ee19
Chang Doe <kontsuyo@users.noreply.github.com>
2025-07-03 09:34:18 +0900

    chore: CORS設定

--- djangoapp/config/settings.py
+++ djangoapp/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "rest_framework.authtoken",
     "posts",
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -172,3 +174,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Viteのデフォルトポート
+    "http://127.0.0.1:5173",  # Viteのデフォルトポート
+]
